ca72771795d24fbd3451b14863020915113dfb86
Jesse Kinser <email": null>
2016-06-14 20:38:17 -0700

    add js and css

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,53 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$('.information-container').hide()
+
+	$('#meatwad').click(function(event){
+		$('#meatwad-info').show()
+	});
+
+	$('#meatwad').dblclick(function(event){
+		$('#meatwad-info').hide()
+	});
+
+	$('#carl').click(function(event){
+		$('#carl-info').show()
+	});
+
+	$('#carl').dblclick(function(event){
+		$('#carl-info').hide()
+	});
+
+	$('#frylock').click(function(event){
+		$('#frylock-info').show()
+	});
+
+	$('#frylock').dblclick(function(event){
+		$('#frylock-info').hide()
+	});
+
+	$('#shake').click(function(event){
+		$('#shake-info').show()
+	});
+
+	$('#shake').dblclick(function(event){
+		$('#shake-info').hide()
+	});
+
+	$('#window').click(function(event){
+		$('#window-info').show()
+	});
+
+	$('#window').dblclick(function(event){
+		$('#window-info').hide()
+	});
+
+	$('#radio').click(function(event){
+		$('#radio-info').show()
+	});
+
+	$('#radio').dblclick(function(event){
+		$('#radio-info').hide()
+	});
+
 });
